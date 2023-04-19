ff9cb5488f1c2f88c07f38ce6eb10a5178b7384d
Luca Rossini <luca.rossini@iit.it>
2023-04-19 12:15:53 +0200

    fixed camera calibration

--- src/camera_calibration.py
+++ src/camera_calibration.py
@@ -1,6 +1,8 @@
 from xbot_interface import xbot_interface as xbot
 from xbot_interface import config_options as co
-from cartesian_interface.pyci_all import *
+from xbot_msgs.msg import JointState
+
+from cartesian_interface.pyci_all import Affine3
 
 import rospy, rospkg
 import trimesh
@@ -10,14 +12,18 @@ import sensor_msgs.point_cloud2 as pc2
 from std_msgs.msg import Header
 from struct import Struct
 import numpy as np
+from scipy.spatial.transform import Rotation
+
+import pyvista as pv
 
 class CameraCalibration:
     def __init__(self, urdf, srdf):
         urdf_folder = rospkg.RosPack().get_path('centauro_urdf')
         self.__mesh = trimesh.load(urdf_folder + '/meshes/BallHand.stl')
-        edges = self.__mesh.edges
-        scene = trimesh.Scene([edges, self.__mesh])
-        scene.show()
+
+        self.goal = None
+
+
 
         opt = co.ConfigOptions()
         opt.set_urdf(urdf)
@@ -39,13 +45,14 @@ class CameraCalibration:
         time = 0.
         start = self.__robot.getJointPositionMap()
         goal = start.copy()
-        goal['j_arm1_1'] = -0.65
-        goal['j_arm1_2'] = 0.33
+        goal['j_arm1_1'] = -0.80
+        goal['j_arm1_2'] = 0.22
         goal['j_arm1_3'] = -0.04
-        goal['j_arm1_4'] = -1.06
-        goal['j_arm1_5'] = -0.11
-        goal['j_arm1_6'] = -0.34
+        goal['j_arm1_4'] = -0.40
+        goal['j_arm1_5'] = 0.61
+        goal['j_arm1_6'] = -1.17
         goal['d435_head_joint'] = -0.42
+
         while time < tf:
             self.__init_robot_position(tf=tf, time=time, start=start, goal=goal)
             time += dt
@@ -58,7 +65,8 @@ class CameraCalibration:
 
         self.__filtered_cloud = []
 
-        rospy.Subscriber('D435_head_camera/depth/color/points', PointCloud2, self.__pc_callback, queue_size=10)
+        # rospy.Subscriber('D435_head_camera/depth/color/points', PointCloud2, self.__pc_callback, queue_size=10)
+        rospy.Subscriber('/D435i/D435i_camera_head/depth/color/points', PointCloud2, self.__pc_callback, queue_size=10)
 
     def setXFilters(self, x_filt):
         if len(x_filt) != 2:
@@ -91,10 +99,40 @@ class CameraCalibration:
         self.__z_filt = z_filt
 
     def getTransformationMatrix(self, frame_id, topic_name):
-        T_init = self.__model.getPose('ball1', frame_id).matrix()
-        self.__mesh.apply_transform(T_init)
-        T, transformed, cost = trimesh.registration.icp(self.__filtered_cloud, self.__mesh)
+        T_init = self.__model.getPose('ball1', frame_id)
+        T_init.translation *= 1000
+        self.__mesh.apply_transform(T_init.matrix())
+
+        # scale point cloud to match mesh size
+        pointcloud = np.array(self.__filtered_cloud)
+        pointcloud = pointcloud * 1000
+        # pointcloud_centroid = pointcloud.mean(axis=0)
+        # mesh_centroid = self.__mesh.vertices.mean(axis=0)
+
+        # align the point cloud centroid to the mesh centroid
+        # pointcloud = pointcloud - pointcloud_centroid + mesh_centroid
+
+        plotter = pv.Plotter()
+        plotter.add_mesh(pv.wrap(self.__mesh), color='white', opacity=0.5)
+        plotter.add_mesh(pv.wrap(pointcloud), color='red', point_size=2)
+        plotter.add_axes_at_origin(xlabel=None, ylabel=None, zlabel=None)
+        T, transformed, cost = trimesh.registration.icp(pointcloud, self.__mesh, scale=False)
         print(T)
+        print(f'scale: {np.linalg.norm(T[:3, 0])}')
+        print(f'rot: {Rotation.from_matrix(T[:3,:3]/np.linalg.norm(T[:3, 0])).as_matrix}')
+        print(f'trans: {T[:3, 3]}')
+
+        trans = Affine3(T[:3, 3] * 0.001, Rotation.from_matrix(T[:3, :3]/np.linalg.norm(T[:3, 0])).as_quat())
+
+        b_T_c = self.__model.getPose(frame_id, 'd435_head_motor')
+        b_trans = b_T_c.linear @ trans.translation
+        b_rot = b_T_c.linear @ trans.linear
+
+        print(f"offset in d435_head_motor frame: xyz = {b_trans}, rpy={Rotation.from_matrix(b_rot).as_euler('xyz').T}")
+
+
+        plotter.add_mesh(pv.wrap(transformed), color='green', point_size=2)
+        plotter.show()
         self.__generate_pointcloud(frame_id, transformed, topic_name=topic_name)
 
 
@@ -104,6 +142,8 @@ class CameraCalibration:
             q[name] = start + (goal - start) * (time / tf)
         self.__robot.setPositionReference(q)
         self.__robot.move()
+        self.__model.syncFrom(self.__robot)
+        self.__model.update()
 
     def __generate_pointcloud(self, frame_id, cloud, topic_name):
         # define the fields for the PointCloud2 msg
@@ -154,8 +194,10 @@ class CameraCalibration:
             self.__filtered_cloud.append(p)
 
         self.__generate_pointcloud(msg.header.frame_id, self.__filtered_cloud, topic_name='filtered_cloud')
+        # self.__generate_pointcloud('D435_head_camera_color_optical_frame', self.__filtered_cloud, topic_name='filtered_cloud')
         print('cloud filtered')
         self.getTransformationMatrix(msg.header.frame_id, topic_name='aligned_cloud')
+        # self.getTransformationMatrix('D435_head_camera_color_optical_frame', topic_name='aligned_cloud')
         print('cloud_aligned')
 
 if __name__ == '__main__':
@@ -165,6 +207,6 @@ if __name__ == '__main__':
     srdf = rospy.get_param('xbotcore/robot_description_semantic')
 
     camera = CameraCalibration(urdf, srdf)
-    camera.setFilters([-0.2, 0.2], [-0.2, 0.2], [0., 1.2])
+    camera.setFilters([-0.2, 0.1], [-0.2, 0.12], [0., 1.2])
 
     rospy.spin()
